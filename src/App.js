96531c9f78db24be97ca956c13f63f69dbba1547
unknown <wojtekantonik9@gmail.com>
2021-01-26 18:02:41 +0100

    create dropbox of matching results

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import React from "react";
-import {BsSearch} from "react-icons/bs";
 
-export default function App() {
-  const handleOnSubmit = e => {
-    e.preventDefault();
-    console.log("asd");
-  };
+import Search from "./Search";
 
+export default function App(props) {
   return (
-    <div className="search">
-      <section className="search__wrapper">
-        <h1 className="search__title">Unsplash</h1>
-        <aside className="search__aside">
-          The internet's source of{" "}
-          <a href="https://unsplash.com/" target="blank_" title="Check out cool images!">
-            freely-usable images
-          </a>
-          <br />
-          Powred by creators everywhere.
-        </aside>
-        <main className="search__main">
-          <form onSubmit={handleOnSubmit} className="search__form">
-            <label htmlFor="search" className="search__search-icon">
-              <BsSearch />
-            </label>
-            <input type="text" id="search" placeholder="Search free high-resolution photos" className="search__search-input" />
-          </form>
-        </main>
-      </section>
+    <div>
+      <Search />
     </div>
   );
 }
