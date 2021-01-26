1e2d14fcd24a0055e6a600b01e11b5d7e1838042
unknown <wojtekantonik9@gmail.com>
2021-01-26 22:14:25 +0100

    create aside of tags

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,11 +1,11 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 
-import {unsplashURL, key, searchTheme} from "../utils/utils";
+import {key, searchTheme} from "../utils/utils";
 import Search from "./Search";
 
 export default function Result(props) {
-  console.log(props.match.params.keyWord);
+  const [tags, setTags] = useState([]);
 
   useEffect(() => {
     axios
@@ -15,14 +15,32 @@ export default function Result(props) {
         },
       })
       .then(res => {
-        console.log(res);
+        //some tags might repeat
+        const allTags = [];
+        res.data.results.map(image => image.tags.map(tag => allTags.push(tag.title)));
+
+        const uniqueTags = Array.from(new Set(allTags));
+        setTags(uniqueTags);
       })
       .catch(err => console.log(err));
-  }, []);
+  }, [props]);
 
   return (
     <div className="result">
       <Search props={props} theme={searchTheme.result} result={props.match.params.keyWord} />
+
+      <h2 className="result__title" onClick={() => console.log(tags[4])}>
+        {props.match.params.keyWord}
+      </h2>
+      <div className="result__related-tags">
+        {tags.length > 0 &&
+          tags.map((tag, id) => (
+            <span className="result__related-tags-tag" key={id}>
+              {tag}
+            </span>
+          ))}
+        <div className="realted-tags-fadeout-effect"></div>
+      </div>
     </div>
   );
 }
