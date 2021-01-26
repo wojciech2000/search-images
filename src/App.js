69af8dea05c6cb6f44a4f2da6fcad45750b9cc0f
unknown <wojtekantonik9@gmail.com>
2021-01-26 16:09:13 +0100

    create view for search component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BsSearch} from "react-icons/bs";
 
-function App() {
+export default function App() {
+  const handleOnSubmit = e => {
+    e.preventDefault();
+    console.log("asd");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="search">
+      <section className="search__wrapper">
+        <h1 className="search__title">Unsplash</h1>
+        <aside className="search__aside">
+          The internet's source of{" "}
+          <a href="https://unsplash.com/" target="blank_" title="Check out cool images!">
+            freely-usable images
+          </a>
+          <br />
+          Powred by creators everywhere.
+        </aside>
+        <main className="search__main">
+          <form onSubmit={handleOnSubmit} className="search__form">
+            <label htmlFor="search" className="search__search-icon">
+              <BsSearch />
+            </label>
+            <input type="text" id="search" placeholder="Search free high-resolution photos" className="search__search-input" />
+          </form>
+        </main>
+      </section>
     </div>
   );
 }
-
-export default App;
